a74995c6eeada63ba5bd1c3575b898ed8f8da8cb
Abatur <alek@zolnowski.eu>
2024-05-18 16:05:06 +0200

    Updated cuckoo hashing methods

--- Projekt3/main.cpp
+++ Projekt3/main.cpp
@@ -4,21 +4,13 @@ using namespace std;
 
 int main()
 {
-    Cuckoo<int, int> x;
-
-    x.insert(1,1);
-    x.insert(1,2);
-    x.insert(3,3);
-    x.insert(4,4);
-    cout<<x.getSize()<<endl;
-    cout<<x.find(0)<<endl;
-    x.display();
-
-    /*HashTable<int, int>* a;
+    
+    HashTable<int, int>* a;
     Separate<int, int> b;
     Open<int, int> c;
+    Cuckoo<int, int> x;
 
-    a = &b;
+    a = &x;
 
     for(int i=0; i<1000000; i++)
     {
@@ -30,16 +22,6 @@ int main()
     timer.find_time(*a, 10000);
     timer.insert_time(*a,12334,51235);
     timer.getSize_time(*a);
-    
-    a = &c;
-    for(int i=0; i<1000000; i++)
-    {
-        a->insert(i,i);
-    }
 
-    timer.find_time(*a, 10000);
-    timer.insert_time(*a,12334,51235);
-    timer.getSize_time(*a);
-    */
     return 0;
 }
